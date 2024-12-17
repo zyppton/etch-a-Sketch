3278dea37c10eb5e5eb4a5bcd13fede905e2ea54
zyppton <jasonconlow@gmail.com>
2024-12-17 08:50:04 -0800

    code cleaned up and works

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-let grid = 256;
-
 const btn = document.querySelector("#btn");
 
 const container = document.createElement("div");
@@ -7,58 +5,47 @@ container.id = "container";
 
 document.body.appendChild(container);
 
-for (i = grid; i > 0; i--) {
-    const square = document.createElement("div");
+function deleteGrid() {
 
-    square.classList.add("square");
-
-    container.appendChild(square);
-};
-
-const gridSquares = document.querySelectorAll(".square");
-
-
-gridSquares.forEach(piece => {
-    piece.addEventListener("mouseover", () => {
-        piece.style.backgroundColor = "red";
-        console.log("hey");
+    const gridSquaresTwo = document.querySelectorAll(".square");
+    gridSquaresTwo.forEach(part => {
+        part.remove();
     });
-});
 
-function createGrid() {
-    let userNumber = prompt("Enter number between 1-100");
+}
 
-    if (userNumber > 0 && userNumber <= 100) {
-        grid = userNumber * userNumber;
-        let squareSize = 600 / userNumber;
-        let squareString = `${squareSize}`;
-        const gridSquaresTwo = document.querySelectorAll(".square");
-        gridSquaresTwo.forEach(part => {
-            part.remove();
+function createGrid(side = 16) {
+    let grid = side * side;
+    let squareSize = 600 / side;
+    let squareString = `${squareSize}`;
+
+    for (i = grid; i > 0; i--) {
+        const square = document.createElement("div");
+    
+        square.classList.add("square");
+        square.style.width = squareString + "px";
+        square.style.height = squareString + "px";
+        container.appendChild(square);
+    };
+    const gridSquaresThree = document.querySelectorAll(".square");
+
+    gridSquaresThree.forEach(piece => {
+        piece.addEventListener("mouseover", () => {
+            piece.style.backgroundColor = "black";
         });
-        
-
-
-        for (i = grid; i > 0; i--) {
-            const square = document.createElement("div");
-        
-            square.classList.add("square");
-            square.style.width = squareString;
-            square.style.height = squareString;
-            container.appendChild(square);
-        };
-        const gridSquaresThree = document.querySelectorAll(".square");
-
-        gridSquaresThree.forEach(piece => {
-            piece.addEventListener("mouseover", () => {
-                piece.style.backgroundColor = "red";
-                console.log("hey");
-            });
-        });
-    }else {
-        alert("Number is not between 0-100 try again")
-    }
+    });
 
 };
 
-btn.addEventListener("click", () => createGrid());
+
+createGrid();
+
+btn.addEventListener("click", () => { 
+    let userNumber = prompt("Enter number between 1-100");
+    if (userNumber > 0 && userNumber <= 100) {
+        deleteGrid();
+        createGrid(userNumber);
+    } else{
+        alert("Number is not between 0-100 try again");
+    }
+});
