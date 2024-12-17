337bcb2915365760f1ffef4593d95f055598716d
zyppton <jasonconlow@gmail.com>
2024-12-17 08:24:45 -0800

    about to break functions out

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const grid = 256;
+let grid = 256;
+
+const btn = document.querySelector("#btn");
 
 const container = document.createElement("div");
 container.id = "container";
@@ -23,3 +25,40 @@ gridSquares.forEach(piece => {
     });
 });
 
+function createGrid() {
+    let userNumber = prompt("Enter number between 1-100");
+
+    if (userNumber > 0 && userNumber <= 100) {
+        grid = userNumber * userNumber;
+        let squareSize = 600 / userNumber;
+        let squareString = `${squareSize}`;
+        const gridSquaresTwo = document.querySelectorAll(".square");
+        gridSquaresTwo.forEach(part => {
+            part.remove();
+        });
+        
+
+
+        for (i = grid; i > 0; i--) {
+            const square = document.createElement("div");
+        
+            square.classList.add("square");
+            square.style.width = squareString;
+            square.style.height = squareString;
+            container.appendChild(square);
+        };
+        const gridSquaresThree = document.querySelectorAll(".square");
+
+        gridSquaresThree.forEach(piece => {
+            piece.addEventListener("mouseover", () => {
+                piece.style.backgroundColor = "red";
+                console.log("hey");
+            });
+        });
+    }else {
+        alert("Number is not between 0-100 try again")
+    }
+
+};
+
+btn.addEventListener("click", () => createGrid());
